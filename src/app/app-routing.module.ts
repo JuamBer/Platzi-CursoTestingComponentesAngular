7fe913d209bd008a37954257aa8e3b92c52d7134
Juan Sáez García <juambersaezgarcia@gmail.com>
2023-03-14 19:28:00 +0100

    test: nativeElement test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ProductsComponent } from './components/products/products.component';
+import { PersonComponent } from './components/person/person.component';
 import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
+import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
   {
     path: 'products',
-    component: ProductsComponent
+    component: ProductsComponent,
   },
   {
     path: 'pico-preview',
-    component: PicoPreviewComponent
-  }
+    component: PicoPreviewComponent,
+  },
+  {
+    path: 'people',
+    component: PersonComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

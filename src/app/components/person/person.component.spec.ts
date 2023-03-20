68137dc9c414869216653feb0f516f4fc6bd75b7
Juan Sáez García <juambersaezgarcia@gmail.com>
2023-03-20 20:51:54 +0100

    test: Componentes con Inputs

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,6 +1,7 @@
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { Person } from 'src/app/models/person.model';
 
 import { PersonComponent } from './person.component';
 
@@ -17,6 +18,7 @@ fdescribe('PersonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PersonComponent);
     component = fixture.componentInstance;
+    component.person = new Person('Juan', 'Saez', 20, 95, 192);
     fixture.detectChanges();
   });
 
@@ -24,29 +26,42 @@ fdescribe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have <p> with Lorem ipsum', () => {
+  it('should have <p> with `Mi altura es ${component.person.height}`', () => {
     const personElement: HTMLElement = fixture.nativeElement;
     const p = personElement.querySelector('p');
-    expect(p?.textContent).toEqual('Lorem ipsum');
+    expect(p?.textContent).toEqual(`Mi altura es ${component.person.height}`);
   });
 
-  it('should have <p> with Lorem ipsum debug', () => {
+  it('should have <p> with `Mi altura es ${component.person.height}` debug', () => {
     const debugElement: DebugElement = fixture.debugElement;
     const p = debugElement.nativeElement.querySelector('p');
-    expect(p?.textContent).toEqual('Lorem ipsum');
+    expect(p?.textContent).toEqual(`Mi altura es ${component.person.height}`);
   });
 
-  it('should have <p> with Lorem ipsum By.css', () => {
+  it('should have <p> with `Mi altura es ${component.person.height}` By.css', () => {
     const personDebug: DebugElement = fixture.debugElement;
     const pDebug: DebugElement = personDebug.query(By.css('p'));
     const pElement: HTMLElement = pDebug.nativeElement;
-    expect(pElement?.textContent).toEqual('Lorem ipsum');
+    expect(pElement?.textContent).toEqual(
+      `Mi altura es ${component.person.height}`
+    );
   });
 
-  it('should have <h3> with "Hola, PersonComponent"</h3>', () => {
+  it('should have a name "juan"', () => {
+    expect(component.person.name).toEqual('Juan');
+  });
+
+  it('should have h3 with `Hola, ${component.person.name}`', () => {
     const personDebug: DebugElement = fixture.debugElement;
     const h3Debug: DebugElement = personDebug.query(By.css('h3'));
     const h3: HTMLElement = h3Debug.nativeElement;
-    expect(h3?.textContent).toEqual('Hola, PersonComponent');
+    expect(h3?.textContent).toEqual(`Hola, ${component.person.name}`);
+  });
+
+  it('should have h3 with `${component.person.name}`', () => {
+    const personDebug: DebugElement = fixture.debugElement;
+    const h3Debug: DebugElement = personDebug.query(By.css('h3'));
+    const h3: HTMLElement = h3Debug.nativeElement;
+    expect(h3?.textContent).toContain(component.person.name);
   });
 });
--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Person } from 'src/app/models/person.model';
 
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.scss']
+  styleUrls: ['./person.component.scss'],
 })
 export class PersonComponent implements OnInit {
+  @Input() person!: Person;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
